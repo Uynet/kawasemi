028db6e91e0635e5077508c36205b9d345cf90b2
uynet <highsate@gmail.com>
2018-01-13 21:29:04 +0900

    敵っぽいもの

--- src/Entity/Mover/Enemy/teki1.js
+++ src/Entity/Mover/Enemy/teki1.js
@@ -1,4 +1,5 @@
 import Mover from '../mover.js';
+import Enmey from './enemy.js';
 import Art from '../../../art.js';
 import CollisionShape from '../../../Collision/collisionShape.js';
 import Collision from '../../../Collision/collision.js';
@@ -8,8 +9,7 @@ import StageEntity from '../../../Stage/stageEntity.js';
 
 export default class Teki1 extends Mover{
   constructor(pos){
-    super(pos);
-    this.type = ENTITY.ENEMY;
+    super(pos,VEC0,VEC0);
     this.sprite = Art.SpriteFactory(Art.teki3Texture);
     this.sprite.position = pos;
     this.collisionShape = new CollisionShape(SHAPE.BOX,new Box(pos,16,16));//衝突判定の形状
@@ -31,8 +31,13 @@ export default class Teki1 extends Mover{
     }
   }
 
+  UpdatePosition(){
+    this.pos.x += this.vel.x;
+    this.pos.y += this.vel.y;
+  }
+
   Update(){
     this.collision();
-    /*nothing to do*/
+    this.UpdatePosition();
   }
 }
