bff8e18449e331a0b07f404c21efb2d4bbe9b42c
uynet <highsate@gmail.com>
2018-01-17 22:54:57 +0900

    wip

--- src/UI/ui.js
+++ src/UI/ui.js
@@ -1,14 +1,16 @@
-import Art from './art.js';
+import Art from '../art.js';
 
 export default class UI{
   constructor(UItexture,type){
     this.type = type;
     switch (type){
+      /*武器アイコン*/
       case 0:
         this.sprite = Art.SpriteFactory(UItexture);
         this.sprite.position.x = 32;
         this.sprite.position.y = 32;
         break;
+        /*セレクトボックス*/
       case 1:
         this.sprite = Art.SpriteFactory(UItexture);
         this.sprite.position.x = 30;
---
+++ src/UI/uiManager.js
@@ -0,0 +1,68 @@
+import Drawer from '../drawer.js';
+import UI from './ui.js';
+import Input from '../input.js';
+import Timer from '../timer.js';
+
+
+
+ /*UIクラス*/
+ /*TODO リファクタリング*/
+ export default class UIManager{
+   static Init(){
+     /*content
+      * HP
+      * 武器
+      * その他
+      */
+     this.UIList = [];//UI全部のリスト
+       this.WeaponIconList = [];//武器アイコンのリスト
+
+   }
+   /*WeaponIconのポップアップ*/
+   static OpenWeapon(){
+     for(let i = 0;i<this.WeaponIconList.length;i++){
+       let to = 32 + 20*i;
+       let dif = to - this.WeaponIconList[i].sprite.x;
+       this.WeaponIconList[i].sprite.x += dif * 0.6;
+     }
+   }
+   /*ポップアップの逆(?)*/
+   static CloseWeapon(){
+     for(let i = 0;i<this.WeaponIconList.length;i++){
+       this.WeaponIconList[i].sprite.x = 32;
+     }
+   }
+
+   /*UIをリストに登録*/
+   static addUI(ui){
+     /*TODO リストの重複を排除*/
+     this.UIList.push(ui); 
+     switch (ui.type){
+       case 0 : 
+         this.WeaponIconList.push(ui);
+         break;
+         //add more...
+         }
+     Drawer.addContainer(ui.sprite,"UI");
+   }
+   /*UIをリストから削除*/
+   static removeUI(ui){
+     let i = this.UIList.indexOf(ui);
+     Drawer.removeContainer(ui.sprite,"UI");
+     this.UIList.splice(i,1);
+   }
+   /*UIの更新*/
+   static Update(){
+     UIManager.OpenWeapon();
+     for(let l of UIManager.UIList){
+       switch (l.type){
+         case 0:
+           /* */
+           break;
+         case 1://カーソル移動
+           l.Update();
+           break;
+       }
+     }
+   }
+ }
