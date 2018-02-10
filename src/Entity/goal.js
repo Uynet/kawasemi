d4d8e53237fb9d9983b6cb8741301f922ab273aa
uynet <highsate@gmail.com>
2018-02-10 10:14:28 +0900

    ゴール直した

--- src/Entity/goal.js
+++ src/Entity/goal.js
@@ -9,7 +9,6 @@ import Game from '../Game.js';
 import GameOverEvent from '../Event/gameOverEvent.js';
 import EventManager from '../Event/eventmanager.js';
 
-let isgoal = false;
 
 export default class Goal extends Entity{
   constructor(pos){
@@ -18,15 +17,16 @@ export default class Goal extends Entity{
     this.sprite = Art.SpriteFactory(Art.wallPattern[1]);
     this.sprite.position = pos;
     this.collider = new Collider(SHAPE.BOX,new Box(pos,16,16));//衝突判定の形状
+    this.isgoal = false;//??
   }
   Update(){
     if(Collision.on(this,EntityManager.player).isHit){
       /*ステージ遷移処理*/
-      if(!isgoal){
+      if(!this.isgoal){
         Game.stage++;
         let g = new GameOverEvent();
         EventManager.eventList.push(g);
-        isgoal = true;
+        this.isgoal = true;
       }
     }
   }
