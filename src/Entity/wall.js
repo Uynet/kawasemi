9d77138f3bb2370a4fba2ed8768fd0cb956dd128
uynet <highsate@gmail.com>
2018-02-17 15:08:46 +0900

    update font

--- src/Entity/wall.js
+++ src/Entity/wall.js
@@ -10,11 +10,14 @@ let VEC0 = {x:0,y:0};
 export default class Wall extends Entity{
   constructor(pos,tex){
     super(pos,{x:0,y:0});
+    /*基本情報*/
+    //this.name = name; 必要になったら
     this.type = ENTITY.WALL;
+    this.collider = new Collider(SHAPE.BOX,new Box(pos,16,16));//衝突判定の形状
+    /*スプライト*/
     this.tex = tex
     this.sprite = Art.SpriteFactory(this.tex);
     this.sprite.position = pos;
-    this.collider = new Collider(SHAPE.BOX,new Box(pos,16,16));//衝突判定の形状
   }
   Update(){
     /*nothing to do*/
