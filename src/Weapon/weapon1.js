45df974296aaca790da3cfac206826b048f294c8
uynet <highsate@gmail.com>
2018-03-01 03:09:46 +0900

    Upgrade optionを選択可能に(雑)

--- src/Weapon/weapon1.js
+++ src/Weapon/weapon1.js
@@ -26,6 +26,10 @@ export default class Weapon1 extends Weapon{
     this.cost = this.param.cost;
     this.speed = this.param.speed;//弾速
     this.length = this.param.length;//射程距離
+    /*option*/
+    this.isTarget = this.param.isTarget;
+    this.isHorming = this.param.isHorming;
+    this.isLasersight = this.param.isLasersight;
   }
   shot(player){
     //最後に撃ってからframeまで停止
@@ -46,7 +50,7 @@ export default class Weapon1 extends Weapon{
           x: player.pos.x -4 + 10 * Math.cos(this.arg),
           y: player.pos.y + 10 * Math.sin(this.arg),
         }
-        let bullet = new Bullet1(p,this.speed,this.arg,this);
+        let bullet = new Bullet1(p,this);
         EntityManager.addEntity(bullet);
         /* ■ SoundEffect : shot */
         /* □ Effect : shot */
@@ -59,6 +63,22 @@ export default class Weapon1 extends Weapon{
         //振動
         //EventManager.eventList.push(new QuakeEvent(8,2));
       }
+    }
+  }
+  Update(player){
+    if(this.isTarget) this.Target(player);
+    if(this.isLasersight) this.Lasersight(player);
+  }
+  Reset(){
+    if(this.isTarget)EntityManager.removeEntity(this.target);
+    if(this.isLasersight)EntityManager.removeEntity(this.lasersight);
+  }
+  Option(option,value){
+    switch(option){
+      case "isHorming" : this.isHorming = value ;break;
+      case "isLasersight" : this.isLasersight = value ;break;
+      case "isTarget" : this.isTarget = value ;break;
+      default : console.warn(option);
     }
   }
 }
