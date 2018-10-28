f09a902e3957139f86301748364fcd0dba98d387
uynet <highsate@gmail.com>
2018-10-28 21:32:16 +0900

    レーザー修正

--- src/Entity/AI/bullet2AI.js
+++ src/Entity/AI/bullet2AI.js
@@ -9,40 +9,12 @@ export default class Bullet2AI{
   constructor(bullet){
     this.bullet = bullet;
   }
-  /* 衝突判定 */
-  Collision(){
-    for(let l of EntityManager.enemyList){
-      if(Collision.on(this.bullet,l).isHit){
-        l.Damage(-RandBET(this.bullet.atkMin,this.bullet.atkMax));
-        this.bullet.hp--;
-        /* ■ SoundEffect : hitWall */
-        /* □ Effect : hitWall */
-      }
-    }
-    for(let l of EntityManager.wallList){
-      if(Collision.on(this.bullet,l).isHit){
-        //breakable object
-          EntityManager.addEntity(new Explosion2(CPV(this.bullet.pos),this.bullet.arg + Math.PI));
-        if(l.isBreakable){
-          /* ■ SoundEffect : hitWood */
-          l.Damage(-RandBET(this.bullet.atkMin,this.bullet.atkMax));
-          this.bullet.hp--;
-          //wall
-          }else{
-            /* ■ SoundEffect : hitWall */
-            this.bullet.hp = 0;
-          /* □ Effect : Exp */
-          }
-      }
-    }
-  }
   Observer(){
     if( this.bullet.frame > 20 || this.bullet.hp<=0){
       EntityManager.removeEntity(this.bullet);
     }
   }
   Do(){
-    this.Collision();
     this.Observer();
     this.bullet.sprite.position = ADV(this.bullet.pos,VECN(8));
     this.bullet.sprite.position.x -=4;
