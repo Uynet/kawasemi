3fb7fb5e9c4c8db77e491666a3d772496a52c779
uynet <highsate@gmail.com>
2020-09-01 14:28:10 +0900

    PIXI.Text

---
+++ src/UI/text.js
@@ -0,0 +1,29 @@
+import UI from "./ui.js";
+
+export default class Text extends UI{
+  constructor(pos, str, style) {
+    super(pos);
+    const defaultStyle = { fontFamily: 'gkktt', fontSize: 50, fill: 0xffffff, align: 'center' };
+    /*基本情報*/
+    this.name = "font";
+    this.isAlive = true; //消えたらfalse
+    this.frame = 0; //stagepopでしか使ってない
+    /*スプライト*/
+    this.str = str; //0~9
+    this.sprite = new PIXI.Text(str,defaultStyle) ;
+    this.sprite.scale.set(0.2);
+    this.sprite.position = copy(this.pos);
+  }
+  ChangeText(text) {
+    this.sprite.updateText(text);
+  }
+  SetPos(pos) {
+    this.pos = copy(pos);
+    this.sprite.position.x = pos.x;
+    this.sprite.position.y = pos.y;
+  }
+  Update() {
+    this.frame++;
+    this.ExecuteEvent();
+  }
+}
